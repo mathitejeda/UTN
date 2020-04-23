447ec2c9c59f00ea83bf28d22c77d90b21723e54
Mathias Tejeda <mathi.tejeda@gmail.com>
2020-04-22 21:08:03 -0300

    Update tipos.h

--- Archivos/tipos.h
+++ Archivos/tipos.h
@@ -1,6 +1,13 @@
 #ifndef TIPOS_H_INCLUDED
 #define TIPOS_H_INCLUDED
 
+struct platos
+{
+ int ID, tiempo_preparacion,ID_restaurante, comision_restaurante,ID_categoria;
+ char nombre;
+ float costo_preparacion,valor_venta;
+ bool estado;
+};
 
 
 #endif // TIPOS_H_INCLUDED
