6553d6dd7b170aeeea676569123659440bc0136a
Mathias Tejeda <mathi.tejeda@gmail.com>
2020-04-23 20:15:55 -0300

    change

--- Archivos/funciones.h
+++ Archivos/funciones.h
@@ -3,4 +3,25 @@
 
 
 
+void cargarplato(platos *p){
+    cout<< "ID del plato: ";
+    cin>> p->ID;
+    cout<< "Nombre: ";
+    cin>> p->nombre;
+    cout << "costo de preparacion: $";
+    cin >> p->costo_preparacion;
+    cout<< "Valor de venta: $";
+    cin>> p->valor_venta;
+    cout<< "tiempo de preparacion: ";
+    cin>> p->tiempo_preparacion;
+    cout<< "ID restaurante: ";
+    cin>> p->ID_restaurante;
+    cout<< "Comision restaurante: ";
+    cin>> p->comision_restaurante;
+    cout<< "ID categoria: ";
+    cin>> p->ID_categoria;
+    p->estado = true;
+}
+
+
 #endif // FUNCIONES_H_INCLUDED
