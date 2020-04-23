b0a7d40b45456b053989075e1220856bf6d3cf03
Mathias Tejeda <mathi.tejeda@gmail.com>
2020-04-22 21:01:56 -0300

    Agregar archivos

---
+++ Archivos/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+using namespace std;
+#include "tipos.h"
+#include "rlutil.h"
+using namespace rlutil;
+#include "funciones.h"
+#include "menus.h"
+
+int main (){
+    int opc;
+    
+    return 0;
+}
+
---
+++ Archivos/menus.h
@@ -0,0 +1,33 @@
+#ifndef MENUS_H_INCLUDED
+#define MENUS_H_INCLUDED
+
+void menu_principal()
+{
+cout << "    MENÚ PRINCIPAL"<<endl;
+cout << "--------------"<<endl;
+cout << "1) PLATOS"<<endl;
+cout << "2) CLIENTES"<<endl;
+cout << "3) PEDIDOS"<<endl;
+cout << "4) REPORTES"<<endl;
+cout << "5) CONFIGURACIÓN"<<endl;
+cout << "----------------------"<<endl;
+cout << "0) SALIR DEL PROGRAMA"<<endl;
+}
+
+void SubmenuPlatos (){
+    
+cout<<"MENU DE PLATOS"<<endl;
+cout<<"------------------"<<endl;
+cout<<"1) NUEVO PLATO"<<endl;
+cout<<"2) MODIFICAR PLATO"<<endl;
+cout<<"3) LISTAR PLATO POR ID"<<endl;
+cout<<"4) PLATOS POR RESTAURANT"<<endl;
+cout<<"4) LISTAR TODOS LOS PLATOS"<<endl;
+cout<<"5) ELIMINAR PLATO"<<endl;
+cout<<"------------------------------"<<endl;
+cout<<"0) VOLVER AL MENÚ PRINCIPAL"<<endl;
+
+
+}
+
+#endif // MENUS_H_INCLUDED
